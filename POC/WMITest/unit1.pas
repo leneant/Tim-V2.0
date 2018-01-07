15a68d15410d16dc7355c5e430324cdd777cf846
Unknown <leneantii@yahoo.fr>
2018-01-07 13:17:38 +0000

    Windows adaptations and tests

--- POC/WMITest/unit1.pas
+++ POC/WMITest/unit1.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
-  Windows,Variants,ActiveX,JwaWbemCli,IU_GeneralUtils;
+  Variants,IU_GeneralUtils;
 
 
 
@@ -32,104 +32,30 @@ implementation
 {$R *.lfm}
 
 
-const
-RPC_C_AUTHN_LEVEL_DEFAULT = 0;
-RPC_C_IMP_LEVEL_IMPERSONATE = 3;
-RPC_C_AUTHN_WINNT = 10;
-RPC_C_AUTHZ_NONE = 0;
-RPC_C_AUTHN_LEVEL_CALL = 3;
-EOAC_NONE = 0;
-
-procedure wmi_LOGICALDISK;
-  const
-  strLocale    = '';
-  strUser      = '';
-  strPassword  = '';
-  strNetworkResource = 'root\cimv2';
-  strAuthority       = '';
-  WQL                = 'SELECT DeviceID,FileSystem FROM Win32_logicaldisk where (DriveType=''3'')';
-  var
-  FWbemLocator         : IWbemLocator;
-  FWbemServices        : IWbemServices;
-  FUnsecuredApartment  : IUnsecuredApartment;
-  ppEnum               : IEnumWbemClassObject;
-  apObjects            : IWbemClassObject;
-  puReturned           : ULONG;
-  pVal1, pVal2         : OleVariant;
-  pType                : Integer;
-  plFlavor             : Integer;
-  Succeed              : HRESULT;
-
-  _currentDir : string;
-  _dirs : string;
-  _dsize, _free : int64;
-  _fsize, _ffree : string;
-
-  begin
-  // Set general COM security levels --------------------------
-  // Note: If you are using Windows 2000, you need to specify -
-  // the default authentication credentials for a user by using
-  // a SOLE_AUTHENTICATION_LIST structure in the pAuthList ----
-  // parameter of CoInitializeSecurity ------------------------
-  if Failed(CoInitializeSecurity(nil, -1, nil, nil, RPC_C_AUTHN_LEVEL_DEFAULT, RPC_C_IMP_LEVEL_IMPERSONATE, nil, EOAC_NONE, nil)) then Exit;
-  // Obtain the initial locator to WMI -------------------------
-  if Succeeded(CoCreateInstance(CLSID_WbemLocator, nil, CLSCTX_INPROC_SERVER, IID_IWbemLocator, FWbemLocator)) then
-  try
-    // Connect to WMI through the IWbemLocator::ConnectServer method
-    if Succeeded(FWbemLocator.ConnectServer(strNetworkResource, strUser, strPassword, strLocale,  WBEM_FLAG_CONNECT_USE_MAX_WAIT, strAuthority, nil, FWbemServices)) then
-    try
-      // Set security levels on the proxy -------------------------
-      if Failed(CoSetProxyBlanket(FWbemServices, RPC_C_AUTHN_WINNT, RPC_C_AUTHZ_NONE, nil, RPC_C_AUTHN_LEVEL_CALL, RPC_C_IMP_LEVEL_IMPERSONATE, nil, EOAC_NONE)) then Exit;
-      if Succeeded(CoCreateInstance(CLSID_UnsecuredApartment, nil, CLSCTX_LOCAL_SERVER, IID_IUnsecuredApartment, FUnsecuredApartment)) then
-      try
-        // Use the IWbemServices pointer to make requests of WMI
-        //Succeed := FWbemServices.ExecQuery('WQL', WQL, WBEM_FLAG_FORWARD_ONLY OR WBEM_FLAG_RETURN_IMMEDIATELY, nil, ppEnum);
-        Succeed := FWbemServices.ExecQuery('WQL', WQL, WBEM_FLAG_FORWARD_ONLY, nil, ppEnum);
-        if Succeeded(Succeed) then
-        begin
-           _currentdir := getCurrentDir;
-           // Get the data from the query
-           while (ppEnum.Next(WBEM_INFINITE, 1, apObjects, puReturned)=0) do
-           begin
-             apObjects.Get('DeviceID', 0, pVal1, pType, plFlavor);
-             apObjects.Get('FileSystem', 0, pVal2, pType, plFlavor);
-             chdir(pVal1);
-             _dsize := disksize(0);
-             _free := diskfree(0);
-             _fsize := IU_realToString(_dsize/1024/1024/1024,2) + ' Go';
-             _ffree := IU_realToString(_free/1024/1024/1024,2) + ' Go';
-             form1.ListBox1.Items.Add(pVal1 + ' -> ' + pVal2 + ' ' + _fsize + ', ' + _ffree);
-             VarClear(pVal1);
-             VarClear(pVal2);
-           end;
-           chdir(_currentdir);
-        end
-        else
-        form1.ListBox1.Items.Add(Format('Error executing WQL sentence %x',[Succeed]));
-      finally
-        FUnsecuredApartment := nil;
-      end;
-    finally
-      FWbemServices := nil;
-    end;
-  finally
-    FWbemLocator := nil;
-  end;
-end;
 
 { TForm1 }
 
 procedure TForm1.Button1Click(Sender: TObject);
+var
+  _driveslist : IU_T_SystemDrivesLists;
+  i : integer ;
+  _dsize, _dfree : int64;
+  _currentdir : string;
+  _fsize, _ffree : string;
 begin
-// Initialize COM. ------------------------------------------
-//if Succeeded(CoInitializeEx(nil, COINIT_MULTITHREADED)) then
-try
-  wmi_LOGICALDISK;;
-finally
-//  CoUninitialize();
+  ListBox1.Clear;
+  _driveslist := wmi_LOGICALDISK;
+  _currentdir := GetCurrentDir;
+  for i := Low(_drivesList) to High(_drivesList) do begin
+      chdir (_drivesList[i].Entry);
+      IU_getCurrentDriveSizes(_dsize, _dfree);
+      _fsize := IU_realToString(_dsize/1024/1024/1024,2);
+      _ffree := IU_realToString(_dfree/1024/1024/1024,2);
+      ListBox1.Items.Add(_driveslist[i].Entry + ' ' + _drivesList[i].SystemFileType +', Total size ' + _fsize + ' Gb, Free room : ' + _ffree + ' Gb');
+  end;
+  chdir(_currentdir);
 end;
 
-end;
 
 end.
 
